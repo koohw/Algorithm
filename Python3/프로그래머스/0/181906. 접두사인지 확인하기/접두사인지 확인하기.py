f88f21a63d2e1fc372e346984ca28c412ed93f99
구희원 <frankh3w@gmail.com>
2025-07-24 13:38:36 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -0,0 +1,10 @@
+def solution(my_string, is_prefix):
+    answer = []
+    
+    for i in range(len(my_string)):
+        answer.append(my_string[0:i+1])
+    
+    if is_prefix in answer:
+        return 1
+    else:
+        return 0
